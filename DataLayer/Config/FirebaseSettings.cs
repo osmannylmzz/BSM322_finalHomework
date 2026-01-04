f7df92429d72feaec8200005338af0cb1a145959
Osman Yılmaz <107924211+osmannylmzz@users.noreply.github.com>
2026-01-04 03:55:02 +0300

    firebase api key removed

--- DataLayer/Config/FirebaseSettings.cs
+++ DataLayer/Config/FirebaseSettings.cs
@@ -4,13 +4,13 @@ namespace DataLayer.Config
 {
     public static class FirebaseSettings
     {
-        public static string ProjectId = "authentication-4a3df";
-        public static string ApiKey = "AIzaSyAZIYo1MWLFoMugLpG3eKJWqL1uxeu3Uwk";
-        public static string AuthDomain = "authentication-4a3df.firebaseapp.com";
+        public static string ProjectId = "";
+        public static string ApiKey = "";
+        public static string AuthDomain = "";
 
         // ÖNEMLİ: FirebaseDatabase.net için Console URL değil, DB root URL gerekir.
         // Format: https://{db-name}.firebaseio.com/
         // Sonunda / olmalı.
-        public static string RealtimeDbUrl = "https://authentication-4a3df-default-rtdb.europe-west1.firebasedatabase.app/";
+        public static string RealtimeDbUrl = "/";
     }
 }
